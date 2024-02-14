020a05b2d9541d6aa76b814e69b103e52d8067de
MD MUSTAFA ALI <116714683+Alimstfa2911@users.noreply.github.com>
2024-02-14 13:52:08 +0000

    operation

--- text.js
+++ text.js
@@ -1,7 +1,26 @@
-console.log("hyy");
+//console.log("hyy");
 let age=23
 let score=null
 
 let myNum = Number(score)
-console.log(typeof age)
-console.log(myNum)
+//console.log(typeof age)
+//console.log(myNum)
+
+// *** Operation **** //
+
+let value = 3
+let negVal = -value
+//console.log(negVal)
+
+//console.log(2+2)
+
+let str1 = "hello"
+let str2 = "my"
+
+let str3 = str1+ str2
+//console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
