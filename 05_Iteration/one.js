b576d80a4b5d54dcea294ed622c07046573b411c
MD MUSTAFA ALI <116714683+Alimstfa2911@users.noreply.github.com>
2024-03-11 19:16:28 +0000

    Loop

---
+++ 05_Iteration/one.js
@@ -0,0 +1,47 @@
+//for
+
+//while
+
+// do while
+
+//Higher Order Loop
+
+/*for of
+
+["","",""]
+[{},{}]
+
+const arr=[1,2,3,4]
+for (const val of arr) {
+    console.log(val);
+}
+
+
+const greetings = "hello wolrd"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+    
+}*/
+
+/* Map
+The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
+
+
+const map=new Map()
+map.set('IN',"India")
+map.set('US',"United States of America")
+
+//console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,':-',value);
+}
+
+const myObj = {
+    'game1': 'NFS',
+    'game2': 'SpiderMan'
+}
+
+for (const [key,value] of myObj) {
+    console.log(key, ':-',value);
+}*/
---
+++ 05_Iteration/two.js
@@ -0,0 +1,23 @@
+/*const myObj = {
+    js : 'javaScript',
+    cpp : 'C++',
+    rb : 'ruby',
+    swift:'Swift by Apple'
+}
+
+for (const key in myObj) {
+   // console.log(key);
+    //console.log(myObj[key]);
+    console.log(`${key} shortcut is for ${myObj[key]}`);
+}
+
+const prog=["js","rb","py"]
+
+for (const key in prog) {
+    console.log(key);
+    console.log(prog[key]);
+}*/
+
+//forEach
+
+const coding=["js","ruby","java","python","cpp"]
