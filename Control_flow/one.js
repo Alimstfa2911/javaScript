741fadb04d70307ea55289dabf9c5564034a3a64
MD MUSTAFA ALI <116714683+Alimstfa2911@users.noreply.github.com>
2024-03-11 17:27:41 +0000

    Control

---
+++ Control_flow/one.js
@@ -0,0 +1,20 @@
+//const score =200
+
+//if(score>100){
+    //let power = "fly" local scope
+    //var power = "fly"global scope
+ //   console.log(`User power : ${power}`);
+//}
+
+//console.log(`User power:${power}`);
+
+
+//const balance =1000
+//if(balance>500) console.log("test"),console.log("test2");
+
+//const userLoggedIn = true
+//const debitCard =true
+
+//if(userLoggedIn && debitCard ){
+ //   console.log("Aloow")
+//}
