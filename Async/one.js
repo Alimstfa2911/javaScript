13bb657dfd9afc4bb30c6fd96435ab76b8b35800
MD MUSTAFA ALI <116714683+Alimstfa2911@users.noreply.github.com>
2024-03-12 12:15:20 +0000

    Project1

---
+++ Async/one.js
@@ -0,0 +1,8 @@
+//JavaScript - Synchronous, Single threaded
+
+//Execution Context -> Execute one line of code ata a time ->> cal stack and Memory Heap
+//Each operation wait for the last one to complete
+
+//Blocking code => Block the flow of the program ->read file sync
+//Non blocking code => Does not block execution => Read file async
+
